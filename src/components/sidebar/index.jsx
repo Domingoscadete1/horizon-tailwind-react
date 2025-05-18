dc29a352cd6c01c6c1f6549102afe934c6862787
Dilsond <dilsond744@gmail.com>
2025-05-18 14:16:26 +0100

    12

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,15 +1,35 @@
 /* eslint-disable */
+import React, { useEffect, useRef } from "react";
 import Links from "./components/Links";
 import SidebarCard from "components/sidebar/componentsrtl/SidebarCard";
 import routes from "routes.js";
 
 const Sidebar = ({ open, onClose }) => {
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (open && sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [open, onClose]);
+
   return (
     <div
+      ref={sidebarRef}
       className={`sm:none duration-175 linear fixed !z-50 flex min-h-full flex-col bg-white pb-10 shadow-2xl shadow-white/5 transition-all dark:!bg-navy-800 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 ${open ? "translate-x-0" : "-translate-x-96"
         }`}
     >
-      <div className={`mx-[80px] mt-[35px] flex items-center`}>
+      <div className="mx-[80px] mt-[35px] flex items-center">
         <div className="mt-1 ml-1 h-2.5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
           DD<span className="font-medium">Store</span>
         </div>
